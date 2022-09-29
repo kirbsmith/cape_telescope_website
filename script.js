f423d9e5183b34d2fc0059861bd0874ded2afde0
Kirby Smith <kirbsmith@gmail.com>
2022-09-29 18:59:43 -0400

    adjust media queries

--- script.js
+++ script.js
@@ -21,32 +21,5 @@ function findAPODDate(){
         loadedImage.setAttribute('height', '800px');
         parentSection.appendChild(loadedImage);
       }
-      // console.log(data.hdurl);
-      // var imageSection = document.createElement("section");
-      // imageSection.setAttribute('id', 'APODSection');
-      // var imageSectionSource = document.getElementById('APODSection')
-      // imageSectionSource.appendChild(imageSection);
-      // var img = document.createElement("img");
-      // img.src = data.hdurl;
-      // img.setAttribute('height', '800px');
-      // if (img) {
-      //   img.src = data.hdurl;
-      // }
-      // else {
-      //   var loadedImage = document.createElement('img');
-      //   loadedImage.src = data.hdurl;
-      //   imageSection.appendChild(loadedImage);
     })
 }
-// img.src = data.hdurl;
-
-// var creatureSection = document.createElement("section")
-//       creatureSection.setAttribute('class', 'card_section_with_information')
-//       var creatureSectionSrc = document.getElementById('card_image_section')
-//       creatureSectionSrc.appendChild(creatureSection)
-//
-//       var img = document.createElement("img")
-//       img.src = data.cards[0].imageUrl
-//       var cardName = data.cards[0].name
-//       // var src = document.getElementById('card_image_section')
-//       creatureSection.appendChild(img)
