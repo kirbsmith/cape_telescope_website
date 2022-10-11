be13219ba32ba6f14ff3983182f72d68b823f62f
Kirby Smith <kirbsmith@gmail.com>
2022-10-11 18:11:03 -0400

    update apod image description

--- script.js
+++ script.js
@@ -9,17 +9,29 @@ function findAPODDate(){
     .then(res => res.json())
     .then(data => {
       var imageUrl = data.hdurl;
+      var descriptionSource = data.explanation;
       var parentSection = document.getElementById('APODSection');
+      var currentParagraph = document.createElement('p');
+      parentSection.appendChild(currentParagraph);
+      var currentDescription = parentSection.querySelector('p');
       var currentImage = parentSection.querySelector('img');
 
-      if (currentImage) {
+      if (currentImage){
         currentImage.src = imageUrl;
-        currentImage.setAttribute('height', '800px');
+        currentImage.setAttribute('height', '600px');
       } else {
         var loadedImage = document.createElement('img');
         loadedImage.src = imageUrl;
-        loadedImage.setAttribute('height', '800px');
+        loadedImage.setAttribute('height', '600px');
         parentSection.appendChild(loadedImage);
       }
+
+      function renewText() {
+        var originalText = currentParagraph;
+        var newText = descriptionSource;
+        document.querySelector('p').textContent = newText;
+      }
+
+      renewText();
     })
 }
